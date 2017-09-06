963102dd34273c4004b17be1c0c2849c3340b46d
Chris Lawton <clawton2008@outlook.com>
2017-09-06 11:39:19 -0500

    Added Ex1E

---
+++ clawton1E1/Main Form.vb
@@ -0,0 +1,25 @@
+﻿'Name               Richardson County
+'Purpose            Calculates the property tax
+'
+'Programmer         Chris Lawton on 9/6/17
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Prepare form for next Calculation
+        txtAssessed.Text = String.Empty
+        lblTax.Text = String.Empty
+        'Send focus to Assessed Value
+        txtAssessed.Focus()
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'Calculates tax on Assessed Value
+        lblTax.Text = Val(txtAssessed.Text) * 0.015
+        lblTax.Text = Format(lblTax.Text, "currency")
+    End Sub
+End Class
