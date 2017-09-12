0eb3701cc0247d365a2a01a9a8f128035b7f4a08
Chris Lawton <clawton2008@outlook.com>
2017-09-11 19:15:22 -0500

    Adding Ex 1F1

---
+++ WindowsApp1/Main Form.vb
@@ -0,0 +1,35 @@
+﻿'Name:          Cranston Berries
+'Purpose:       Calculates Projected Sales
+'Programmer:    Chris Lawton 9-11-17
+
+Option Strict On
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+        'Close Application
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        lblStrawberriesProjected.Text = Format(Val(txtStrawberriesCurrent.Text) + (Val(txtProjectedincrease.Text) * Val(txtStrawberriesCurrent.Text)), "currency")
+        lblBlueberriesProjected.Text = Format(Val(txtBlueberriesCurrent.Text) + (Val(txtProjectedincrease.Text) * Val(txtBlueberriesCurrent.Text)), "currency")
+        lblRaspberriesProjected.Text = Format(Val(txtRaspberriesCurrent.Text) + (Val(txtProjectedincrease.Text) * Val(txtRaspberriesCurrent.Text)), "currency")
+        'Calculate Projected Sales for Strawberries, Blueberries and Raspberries
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtProjectedincrease.Text = String.Empty
+        txtProjectedincrease.Text = CType(0.05, String)
+        txtStrawberriesCurrent.Text = String.Empty
+        txtBlueberriesCurrent.Text = String.Empty
+        txtRaspberriesCurrent.Text = String.Empty
+        lblStrawberriesProjected.Text = String.Empty
+        lblBlueberriesProjected.Text = String.Empty
+        lblRaspberriesProjected.Text = String.Empty
+        txtProjectedincrease.Focus()
+        'Clear form and reset Projected increase to 0.05 and shift focus back that text box
+
+    End Sub
+End Class
---
+++ clawton1F1/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace clawton1F1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProjectedincrease = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCurrentStrawberries = new System.Windows.Forms.TextBox();
+            this.txtCurrentBlueberries = new System.Windows.Forms.TextBox();
+            this.txtCurrentRaspberries = new System.Windows.Forms.TextBox();
+            this.txtProjectedStrawberries = new System.Windows.Forms.TextBox();
+            this.txtProjectedBlueberries = new System.Windows.Forms.TextBox();
+            this.txtProjectedRaspberries = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::clawton1F1.Properties.Resources.StrawBlueRaspberries;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(435, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Projected &increase % (in decimal form):";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtProjectedincrease
+            // 
+            this.txtProjectedincrease.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProjectedincrease.Location = new System.Drawing.Point(633, 56);
+            this.txtProjectedincrease.Name = "txtProjectedincrease";
+            this.txtProjectedincrease.Size = new System.Drawing.Size(100, 34);
+            this.txtProjectedincrease.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(138, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "&Strawberries:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(143, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "&Blueberries:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(140, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "&Raspberries:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(335, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Current Sales";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(584, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Projected Sales";
+            // 
+            // txtCurrentStrawberries
+            // 
+            this.txtCurrentStrawberries.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCurrentStrawberries.Location = new System.Drawing.Point(367, 199);
+            this.txtCurrentStrawberries.Name = "txtCurrentStrawberries";
+            this.txtCurrentStrawberries.Size = new System.Drawing.Size(100, 34);
+            this.txtCurrentStrawberries.TabIndex = 3;
+            // 
+            // txtCurrentBlueberries
+            // 
+            this.txtCurrentBlueberries.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCurrentBlueberries.Location = new System.Drawing.Point(367, 296);
+            this.txtCurrentBlueberries.Name = "txtCurrentBlueberries";
+            this.txtCurrentBlueberries.Size = new System.Drawing.Size(100, 34);
+            this.txtCurrentBlueberries.TabIndex = 5;
+            // 
+            // txtCurrentRaspberries
+            // 
+            this.txtCurrentRaspberries.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCurrentRaspberries.Location = new System.Drawing.Point(367, 393);
+            this.txtCurrentRaspberries.Name = "txtCurrentRaspberries";
+            this.txtCurrentRaspberries.Size = new System.Drawing.Size(100, 34);
+            this.txtCurrentRaspberries.TabIndex = 7;
+            // 
+            // txtProjectedStrawberries
+            // 
+            this.txtProjectedStrawberries.BackColor = System.Drawing.SystemColors.Control;
+            this.txtProjectedStrawberries.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProjectedStrawberries.Location = new System.Drawing.Point(632, 199);
+            this.txtProjectedStrawberries.Name = "txtProjectedStrawberries";
+            this.txtProjectedStrawberries.Size = new System.Drawing.Size(100, 34);
+            this.txtProjectedStrawberries.TabIndex = 14;
+            // 
+            // txtProjectedBlueberries
+            // 
+            this.txtProjectedBlueberries.BackColor = System.Drawing.SystemColors.Control;
+            this.txtProjectedBlueberries.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProjectedBlueberries.Location = new System.Drawing.Point(633, 296);
+            this.txtProjectedBlueberries.Name = "txtProjectedBlueberries";
+            this.txtProjectedBlueberries.Size = new System.Drawing.Size(100, 34);
+            this.txtProjectedBlueberries.TabIndex = 15;
+            // 
+            // txtProjectedRaspberries
+            // 
+            this.txtProjectedRaspberries.BackColor = System.Drawing.SystemColors.Control;
+            this.txtProjectedRaspberries.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProjectedRaspberries.Location = new System.Drawing.Point(633, 393);
+            this.txtProjectedRaspberries.Name = "txtProjectedRaspberries";
+            this.txtProjectedRaspberries.Size = new System.Drawing.Size(100, 34);
+            this.txtProjectedRaspberries.TabIndex = 16;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(584, 474);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(137, 53);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(405, 474);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(137, 53);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "C&lear Screen";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.Location = new System.Drawing.Point(226, 474);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(137, 53);
+            this.btnPrint.TabIndex = 9;
+            this.btnPrint.Text = "&Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.Location = new System.Drawing.Point(47, 473);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(137, 53);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "&Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 567);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtProjectedRaspberries);
+            this.Controls.Add(this.txtProjectedBlueberries);
+            this.Controls.Add(this.txtProjectedStrawberries);
+            this.Controls.Add(this.txtCurrentRaspberries);
+            this.Controls.Add(this.txtCurrentBlueberries);
+            this.Controls.Add(this.txtCurrentStrawberries);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProjectedincrease);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Cranston Berries";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProjectedincrease;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCurrentStrawberries;
+        private System.Windows.Forms.TextBox txtCurrentBlueberries;
+        private System.Windows.Forms.TextBox txtCurrentRaspberries;
+        private System.Windows.Forms.TextBox txtProjectedStrawberries;
+        private System.Windows.Forms.TextBox txtProjectedBlueberries;
+        private System.Windows.Forms.TextBox txtProjectedRaspberries;
+        internal System.Windows.Forms.Button btnExit;
+        internal System.Windows.Forms.Button btnClear;
+        internal System.Windows.Forms.Button btnPrint;
+        internal System.Windows.Forms.Button btnCalc;
+    }
+}
+
